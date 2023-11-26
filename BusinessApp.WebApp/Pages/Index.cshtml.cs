42d1469b5b6a5cfc4234275bc2f5c7e21024d94e
DuHs1234 <jakob.poelzl@gmail.com>
2023-11-26 17:12:39 +0100

    DuHs1234

--- BusinessApp.WebApp/Pages/Index.cshtml.cs
+++ BusinessApp.WebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessApp.Application.Model;
+using BusinessApp.WebApp.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MongoDB.Driver;
+using static BusinessApp.Application.Infrastructure.BueroMongoContext;
 
 namespace BusinessApp.WebApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        Service service;
+
+        public List<Person> persons { get; set; } = new();
+        List<MongoPerson> MongoPersons = new();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        public IndexModel(Service _service)
+        {
+            service = _service;
+            persons = service.BueroContext.Personen.ToList();
+            MongoPersons = service.BueroMongoContext.Personen.Find(_ => true).ToList();
+        }
+
         public void OnGet()
         {
 
+        }
+
+        public void changeFilter(int filter)
+        {
+            
         }
     }
 }
