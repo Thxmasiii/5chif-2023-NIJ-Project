6639e505b8240026abffbc14f41f5f694cee66ab
Thxmasiii <thomas.woelkart2005@gmail.com>
2023-11-30 11:54:22 +0100

    DTOsa

--- BusinessApp.WebAPI/DTO/BueroDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿using BusinessApp.Application.Model;
-
-namespace BusinessApp.WebAPI.DTO
-{
-    public record BueroDTO(int Id, string adresse, string pLZ, List<LocalRaumDTO> raeume, List<LocalParkplatzDTO> parkplaetze);
-    public record LocalBueroDTO(int Id, string adresse, string plz);
-}
-
